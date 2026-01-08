f3bfb98a9ee985cb9323731cd3d2c08f52b774b4
Samer <newsamer123@gmail.com>
2026-01-08 03:27:14 +0300

    fix: arabic font bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import "./i18n";
 import NotFound from './sections/NotFound.tsx'
+import '@fontsource/cairo/400.css';
+import '@fontsource/cairo/600.css';
+import '@fontsource/cairo/700.css';
+import '@fontsource/playfair-display/400.css';
+import '@fontsource/playfair-display/700.css';
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
